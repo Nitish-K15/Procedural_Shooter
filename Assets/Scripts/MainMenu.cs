913a75bd6adb6b9f8886d153f0a9dc514f36aeaf
Nitish-K15 <79478893+Nitish-K15@users.noreply.github.com>
2022-01-06 02:41:47 +0530

    Main Menu Done

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject controlsPanel, storyPanel,titleScreen;
+   public void Play()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OpenControls()
+    {
+        controlsPanel.SetActive(true);
+        titleScreen.SetActive(false);
+    }
+
+    public void OpenStory()
+    {
+        storyPanel.SetActive(true);
+        titleScreen.SetActive(false);
+    }
+
+    public void GoBack()
+    {
+        storyPanel.SetActive(false);
+        controlsPanel.SetActive(false);
+        titleScreen.SetActive(true);
+    }
+
+    public void End()
+    {
+        Application.Quit();
+    }
+}
